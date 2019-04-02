b7cd9ff9b102ad6f4c4b6ef33ba0d59119dbe0aa
Nicolas Koch <nioko1337@gmail.com>
2019-04-02 11:57:39 +0200

    Improve error handling in example to not make CI fail

--- examples/compare.rs
+++ examples/compare.rs
@@ -8,9 +8,16 @@ fn main() {
     let mut base_file = fs::File::create("base.txt").unwrap();
     let mut src = io::repeat(65).take(100 * 1024 * 1024); // 100 MB
     io::copy(&mut src, &mut base_file).unwrap();
-    
+
     let before_reflink = Instant::now();
-    reflink::reflink("base.txt", "reflinked.txt").unwrap();
+    match reflink::reflink("base.txt", "reflinked.txt") {
+        Ok(()) => {},
+        Err(e) => {
+            println!("Error during reflinking:\n{:?}", e);
+            fs::remove_file("base.txt").unwrap();
+            return;
+        }
+    };
     println!("Time to reflink: {:?}", Instant::now() - before_reflink);
 
     let before_copy = Instant::now();
