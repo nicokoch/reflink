ea54346c992d695de57ed66b9628910abb1bad60
Jiahao XU <Jiahao_XU@outlook.com>
2023-10-04 10:15:38 +1000

    Optimization: Rm dep `libc` (#30)

--- src/sys/unix/linux.rs
+++ src/sys/unix/linux.rs
@@ -1,9 +1,14 @@
-use std::{convert::TryInto, fs, io, mem::size_of, os::unix::io::AsRawFd, path::Path};
+use std::{
+    convert::TryInto,
+    fs, io,
+    mem::size_of,
+    os::{raw::c_int, unix::io::AsRawFd},
+    path::Path,
+};
 
-use ioctl_sys::iow;
-use libc::c_int;
+use ioctl_sys::{ioctl, iow};
 
-use super::super::utility::AutoRemovedFile;
+use crate::sys::utility::AutoRemovedFile;
 
 const C_INT_SIZE: usize = size_of::<c_int>();
 const FICLONE: u32 = iow!(0x94, 9, C_INT_SIZE);
@@ -16,7 +21,7 @@ pub fn reflink(from: &Path, to: &Path) -> io::Result<()> {
 
     let ret = unsafe {
         // http://man7.org/linux/man-pages/man2/ioctl_ficlonerange.2.html
-        libc::ioctl(
+        ioctl(
             dest.as_raw_fd(),
             FICLONE.try_into().unwrap(),
             src.as_raw_fd(),
